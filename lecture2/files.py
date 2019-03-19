8112378a3211e186c248baaae1a7db342f4d5caa
Alexei <48149971+AlexeSemenenko@users.noreply.github.com>
2019-03-19 19:07:18 +0300

    Add files via upload

--- lecture2/files.py
+++ lecture2/files.py
@@ -1,1 +1,25 @@
+def tribonacci(n):
+    a, b, c = 0, 0, 1
+    for i in range(n):
+        yield a
+        a, b, c = b, c, a + b + c
 
+
+if __name__ == '__main__':
+
+    name1 = input("Введите имя файла, из которого нужно считать: ")
+    f1 = open(name1, "r")
+
+    num = int(f1.read())
+
+    f1.close()
+
+    name2 = input("Введите имя файла, в который нужно записать: ")
+    f2 = open(name2, "w")
+
+    count = 1
+    for i in tribonacci(num):
+        f2.write(str(count) + " : " + str(i) + '\n')
+        count += 1
+
+    f2.close()
