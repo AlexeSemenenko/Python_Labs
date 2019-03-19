c5f33aba3978c569e0366f858954520b5426a410
Alexei <48149971+AlexeSemenenko@users.noreply.github.com>
2019-03-19 17:45:58 +0300

    Add files via upload

--- lecture2/tribonacci_def.py
+++ lecture2/tribonacci_def.py
@@ -1,1 +1,12 @@
+def tribonacci(n):
+    a = [0, 0, 1]
+    for i in range(3, n):
+        a.append(a[i - 3] + a[i - 2] + a[i - 1])
+    return a
 
+
+if __name__ == '__main__':
+
+    n = int(input("Введите n: "))
+    arr = tribonacci(n)
+    print(arr)
