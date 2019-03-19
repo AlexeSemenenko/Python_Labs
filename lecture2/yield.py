759f6d7e38a879dcdf9071574e904b83fd3d864f
Alexei <48149971+AlexeSemenenko@users.noreply.github.com>
2019-03-19 18:04:43 +0300

    Add files via upload

--- lecture2/yield.py
+++ lecture2/yield.py
@@ -1,1 +1,12 @@
+def tribonacci(n):
+    a, b, c = 0, 0, 1
+    for i in range(n):
+        yield a
+        a, b, c = b, c, a + b + c
 
+
+if __name__ == '__main__':
+
+    n = int(input("Введите n: "))
+    for i in tribonacci(n):
+        print(i)
