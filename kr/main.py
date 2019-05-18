c2c5957e78c918b8b3e7594af3c925cb8f4c9d24
Alexei <48149971+AlexeSemenenko@users.noreply.github.com>
2019-05-18 12:34:42 +0300

    Add files via upload

--- kr/main.py
+++ kr/main.py
@@ -1,1 +1,9 @@
+file = open('input.txt', 'r')
+string = sorted(list(file.read().split()), reverse=True)
+file.close()
+biggest_s = ''
+for i in string:
+    biggest_s += i
+print('The biggest number that we can make is', biggest_s)
+
 
